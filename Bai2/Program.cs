d52e129690342b9c46f3156a04e3ca73ea40a999
QuangNguyenTheChick <quangkaka0702@gmail.com>
2018-03-09 17:51:27 +0700

    Ready

---
+++ Bai2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Bai2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hostName = Dns.GetHostName();
+            IPHostEntry iplc = Dns.Resolve(hostName);
+            foreach (IPAddress i in iplc.AddressList)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
